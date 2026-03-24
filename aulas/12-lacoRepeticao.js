4bef80113ab20c3a0ce43b87f4a1b783e8f772bf
lucasoliveiratofaneli <lucasoliveirasenaijau@gmail.com>
2026-03-24 10:12:43 -0300

    Aprendemos como utilizar incrementos e decremenetos

---
+++ aulas/12-lacoRepeticao.js
@@ -0,0 +1,6 @@
+//                  Laço de Repetição - FOR (Para)
+// O professor te colocou de castigo e pediu para você escrever mil vezes a frase
+// "Eu vou prestar atenção nas aulas e anotar tudo!"
+
+for (let i = 0; i < 1000; i++) { // i++ == i = i+1
+}
