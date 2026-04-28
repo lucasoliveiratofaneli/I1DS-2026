c84809e22fabb700169ab03c0ece4eec8f45c0a6
lucasoliveiratofaneli <lucasoliveirasenaijau@gmail.com>
2026-04-28 16:57:15 -0300

    Falhei na realização da atividade

---
+++ Atividade-HTML/js/scripts.js
@@ -0,0 +1,28 @@
+let userCount = 0;
+
+const cadastrar = (event) => {
+  event.preventDefault();
+
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let lista = document.getElementById("lista");
+
+  userCount++;
+  let id = userCount;
+
+  let novoItem = document.createElement("li");
+
+  novoItem.innerHTML =
+    id + " - " + nome + " - " + email +
+    ' <button onclick="editar(this)">Editar</button>' +
+    ' <button onclick="deletar(this)">Deletar</button>';
+
+  lista.appendChild(novoItem);
+
+  atualizarContador();
+
+  // limpar campos 
+  document.getElementById("nome").value = "";
+  document.getElementById("email").value = "";
+
+};
